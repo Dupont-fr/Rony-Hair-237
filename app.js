fd91d42def967c44e80ececbdacfa2a87f1feaf7
Djeague Prince Dupont <dupontdjeague@gmail.com>
2026-02-24 12:55:50 +0100

    Update dist and reviewController

--- app.js
+++ app.js
@@ -19,8 +19,10 @@ app.use(
   }),
 )
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
+// app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 
 mongoose
   .connect(process.env.MONGODB_URI)
