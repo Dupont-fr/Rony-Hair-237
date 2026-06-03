749b99feff2dbe43245fcca343cbda2219578c4f
Djeague Prince Dupont <dupontdjeague@gmail.com>
2026-06-03 12:42:27 +0100

    fix 2.1.0

---
+++ dist/assets/publicService-BJviYKzA.js
@@ -0,0 +1,1 @@
+import{b as s,A as a}from"./index-ZuFYN-2u.js";const r=async()=>(await s.get(a.categories)).data,n=async e=>(await s.get(`${a.categories}/${e}`)).data;export{n as a,r as g};
--- dist/assets/publicService-CkmLUB96.js
+++
@@ -1,1 +0,0 @@
-import{b as s,A as a}from"./index-DI-kcNQk.js";const r=async()=>(await s.get(a.categories)).data,n=async e=>(await s.get(`${a.categories}/${e}`)).data;export{n as a,r as g};
